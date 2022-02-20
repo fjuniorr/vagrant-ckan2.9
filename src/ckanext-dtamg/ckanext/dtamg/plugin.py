82d4d517728623ba8eb3ca9d07b1d2ea6c9a04fc
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2022-02-20 15:15:09 -0300

    empty home page

--- src/ckanext-dtamg/ckanext/dtamg/plugin.py
+++ src/ckanext-dtamg/ckanext/dtamg/plugin.py
@@ -5,4 +5,13 @@ import ckan.plugins.toolkit as toolkit
 class DtamgPlugin(plugins.SingletonPlugin):
     """An example plugin
     """
-    pass
+    # Declare that this class implements IConfigurer.
+    plugins.implements(plugins.IConfigurer)
+
+    def update_config(self, config):
+
+        # Add this plugin's templates dir to CKAN's extra_template_paths, so
+        # that CKAN will use this plugin's custom templates.
+        # 'templates' is the path to the templates dir, relative to this
+        # plugin.py file.
+        toolkit.add_template_directory(config, 'templates')
